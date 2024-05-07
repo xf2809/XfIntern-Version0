13a51eb317b8a1f37fde58bf9eafa024ed48bb4f
manzil-infinity180 <rahulvs2809@gmail.com>
2024-05-07 22:54:00 +0530

    changes

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const cors = require('cors');
 const cloudinary = require("cloudinary").v2;
 app.use(cookieParser());
 app.use(express.json());
-
+// changes for direct contribution
 app.use(cors({
   // origin: ["https://frontend-anchors.onrender.com"],
   // origin:["https://frontend-anchors.onrender.com/","http://localhost:5173"],
